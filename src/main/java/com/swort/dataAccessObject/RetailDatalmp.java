55d16bd1a57809eaaa5e12ede8a3b433e39c9dbd
adeluyemi79 <yhyme2reall@gmail.com>
2024-01-04 21:06:01 +0000

    source code

---
+++ src/main/java/com/swort/dataAccessObject/RetailDatalmp.java
@@ -0,0 +1,51 @@
+package com.swort.dataAccessObject;
+
+
+
+import java.util.HashMap;
+
+import java.util.Map;
+
+import com.swort.RetailModule;
+
+import com.swort.dataAccessObject.RetailAccessObject;
+
+
+
+
+
+public class RetailDataImp implements RetailAccessObject {
+
+
+
+	Map<Integer,RetailModule> users = new HashMap<>();
+
+	
+
+	
+
+	@Override
+
+	public void create(RetailModule product) {
+
+		users.put(product.getProduct_id(),product);
+
+	}
+
+
+
+	@Override
+
+	public RetailModule read(int product_id) {
+
+		return users.get(product_id);
+
+	}
+
+	
+
+
+
+}
+
+
